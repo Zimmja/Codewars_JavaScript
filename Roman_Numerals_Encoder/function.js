27380bc550fc5923099d63e484e3e7141d13af78
Matt Zimmer <zimmermja@gmail.com>
2021-12-05 12:33:29 +0000

    RNE: tests working up to 19

--- Roman_Numerals_Encoder/function.js
+++ Roman_Numerals_Encoder/function.js
@@ -1,18 +1,12 @@
 const solution = (number) => {
-  let numerals = [
-    "",
-    "I",
-    "II",
-    "III",
-    "IV",
-    "V",
-    "VI",
-    "VII",
-    "VIII",
-    "IX",
-    "X",
-  ];
-  return numerals[number];
+  numeral = [];
+  if (number >= 10) {
+    number -= 10;
+    numeral.push("X");
+  }
+  let smallNums = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  numeral.push(smallNums[number]);
+  return numeral.join("");
 };
 
 module.exports = solution;
--- Roman_Numerals_Encoder/function.test.js
+++ Roman_Numerals_Encoder/function.test.js
@@ -4,12 +4,50 @@ const checkSymbol = (num, sym) => {
   expect(solution(num)).toBe(sym);
 };
 
+const numerals1_to_10 = [
+  "I",
+  "II",
+  "III",
+  "IV",
+  "V",
+  "VI",
+  "VII",
+  "VIII",
+  "IX",
+  "X",
+];
+const numerals10_to_19 = [
+  "XI",
+  "XII",
+  "XIII",
+  "XIV",
+  "XV",
+  "XVI",
+  "XVII",
+  "XVIII",
+  "XIX",
+];
+
+const numberRange = (max) => {
+  accum = [];
+  for (i = 1; i <= max; i++) {
+    accum.push(i);
+  }
+  return accum;
+};
+
 test("It returns the symbol for 1", () => {
   checkSymbol(1, "I");
 });
 
 test("It returns symbols up to 10", () => {
-  let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let syms = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+  let nums = numberRange(10);
+  let syms = numerals1_to_10;
+  nums.forEach((x, i) => checkSymbol(x, syms[i]));
+});
+
+test("It returns symbols up to 19", () => {
+  let nums = numberRange(19);
+  let syms = numerals1_to_10.concat(numerals10_to_19);
   nums.forEach((x, i) => checkSymbol(x, syms[i]));
 });
