ff0cc78f58705bdf0be2aa3dae20b4dcea953fa1
Matt Zimmer <zimmermja@gmail.com>
2021-12-07 22:48:21 +0000

    all tests passing

--- Replace_With_Alphabet_Position/function.js
+++ Replace_With_Alphabet_Position/function.js
@@ -1,14 +1,18 @@
 function alphabetPosition(text) {
   let letters = text.split("");
-  let numbers = letters.map((letter) => {
-    return getLetterPosition(letter.toLowerCase());
-  });
+  let numbers = convertLettersToNumbers(letters);
   return filterAndJoin(numbers);
 }
 
 function filterAndJoin(numbers) {
   let filtered = numbers.filter((number) => number != "");
   return filtered.join(" ");
+}
+
+function convertLettersToNumbers(letters) {
+  return letters.map((letter) => {
+    return getLetterPosition(letter.toLowerCase());
+  });
 }
 
 function getLetterPosition(letter) {
@@ -18,5 +22,3 @@ function getLetterPosition(letter) {
 }
 
 module.exports = alphabetPosition;
-
-alphabetPosition("the");
