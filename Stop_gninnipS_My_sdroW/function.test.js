ace423ad901c277928d5b03ef3586d48b418a4d4
Matt Zimmer <zimmermja@gmail.com>
2021-12-07 21:55:56 +0000

    4th and 5th tests passing

--- Stop_gninnipS_My_sdroW/function.test.js
+++ Stop_gninnipS_My_sdroW/function.test.js
@@ -8,6 +8,14 @@ test("Flips longer words", () => {
   expect(spinWords("longer")).toBe("regnol");
 });
 
-test("Works with multiple words", () => {
+test("Works with multiple smaller words", () => {
   expect(spinWords("test test")).toBe("test test");
+});
+
+test("Works with multiple longer words", () => {
+  expect(spinWords("longer longer")).toBe("regnol regnol");
+});
+
+test("Works with sentences", () => {
+  expect(spinWords("Stop Spinning My Words")).toBe("Stop gninnipS My sdroW");
 });
