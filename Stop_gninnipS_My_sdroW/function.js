8b5b3e3735049f5e4a98db74721653bb830a706c
Matt Zimmer <zimmermja@gmail.com>
2021-12-07 21:50:32 +0000

    second test passing

--- Stop_gninnipS_My_sdroW/function.js
+++ Stop_gninnipS_My_sdroW/function.js
@@ -1,5 +1,11 @@
 function spinWords(string) {
+  if (string.length > 4) return reverse(string);
   return string;
+}
+
+function reverse(string) {
+  let revArr = string.split("");
+  return revArr.reverse().join("");
 }
 
 module.exports = spinWords;
--- Stop_gninnipS_My_sdroW/function.test.js
+++ Stop_gninnipS_My_sdroW/function.test.js
@@ -3,3 +3,7 @@ const spinWords = require("./function");
 test("Doesn't flip smaller words", () => {
   expect(spinWords("test")).toBe("test");
 });
+
+test("Flips longer words", () => {
+  expect(spinWords("longer")).toBe("regnol");
+});
