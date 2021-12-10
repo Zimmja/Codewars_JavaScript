94586fd65e9fe2fbdc90a387a5654cac6cd4b16c
Matt Zimmer <zimmermja@gmail.com>
2021-12-10 18:53:21 +0000

    rewrote function names to make clearer

--- Human_Readable_Time/function.js
+++ Human_Readable_Time/function.js
@@ -1,24 +1,25 @@
 function humanReadable(seconds) {
   let timeBlocks = [3600, 60, 1];
-  return timeBlocks.map((block) => findReadable(seconds, block)).join(":");
+  return timeBlocks.map((bl) => formattedBlockCount(seconds, bl)).join(":");
 }
 
-function findReadable(s, b) {
-  return `${formatTime(convertTime(s, b))}`;
+function formattedBlockCount(seconds, block) {
+  let blocks = blockCount(seconds, block);
+  return `${formatBlock(blocks)}`;
 }
 
-function convertTime(time, divideBy) {
-  if (divideBy < 3600) time -= subtractionValue(time, 3600);
-  if (divideBy < 60) time -= subtractionValue(time, 60);
+function formatBlock(time) {
+  return `${time < 10 ? "0" : ""}${time}`;
+}
+
+function blockCount(time, block) {
+  if (block < 3600) time -= subtractionValue(time, 3600);
+  if (block < 60) time -= subtractionValue(time, 60);
   return parseInt(time / divideBy);
 }
 
 function subtractionValue(time, sub) {
   return sub * parseInt(time / sub);
-}
-
-function formatTime(time) {
-  return `${time < 10 ? "0" : ""}${time}`;
 }
 
 module.exports = humanReadable;
