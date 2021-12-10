e88999d6bafa27c6f67cde2afa55c833aae03377
Matt Zimmer <zimmermja@gmail.com>
2021-12-10 18:41:04 +0000

    refactored to something more pleasant

--- Human_Readable_Time/function.js
+++ Human_Readable_Time/function.js
@@ -1,15 +1,23 @@
 function humanReadable(seconds) {
-  let times = [];
-  for (let i = 3600; i >= 1; i /= 60) {
-    let time = parseInt(seconds / i);
-    times.push(time);
-    seconds -= time * i;
-  }
-  return `${formTime(times[0])}:${formTime(times[1])}:${formTime(times[2])}`;
+  return [
+    `${formatTime(convertTime(seconds, 3600))}`,
+    `${formatTime(convertTime(seconds, 60))}`,
+    `${formatTime(convertTime(seconds, 1))}`,
+  ].join(":");
 }
 
-function formTime(time) {
+function convertTime(time, divideBy) {
+  if (divideBy < 3600) time -= 3600 * parseInt(time / 3600);
+  if (divideBy < 60) time -= 60 * parseInt(time / 60);
+  return parseInt(time / divideBy);
+}
+
+function formatTime(time) {
   return `${time < 10 ? "0" : ""}${time}`;
 }
 
 module.exports = humanReadable;
+
+// console.log(seconds(3600, 1));
+// console.log(seconds(3600, 60));
+// console.log(seconds(3600, 3600));
