104c26c970f046771e46d7652c8574e02367f88e
Matt Zimmer <zimmermja@gmail.com>
2021-12-12 12:00:26 +0000

    first two tests passing, including: It returns false if not enough numbers are given

--- 6kyu_IP_Validation/function.js
+++ 6kyu_IP_Validation/function.js
@@ -1,5 +1,6 @@
 function isValidIP(str) {
-  return false;
+  let parts = str.split(".");
+  return parts.length === 4;
 }
 
 module.exports = isValidIP;
--- 6kyu_IP_Validation/function.test.js
+++ 6kyu_IP_Validation/function.test.js
@@ -1,5 +1,9 @@
 const isValidIP = require("./function");
 
-test("It returns false", () => {
-  expect(isValidIP()).toEqual(false);
+test("It returns true when IP = 0.0.0.0", () => {
+  expect(isValidIP("0.0.0.0")).toBe(true);
+});
+
+test("It returns false if not enough numbers are given", () => {
+  expect(isValidIP("0.0.0")).toBe(false);
 });
