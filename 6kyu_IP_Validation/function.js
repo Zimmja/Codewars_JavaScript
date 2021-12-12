7161356870839ed421a63c4c6bb38f2cbb9c0002
Matt Zimmer <zimmermja@gmail.com>
2021-12-12 12:06:34 +0000

    passing test: Returns false when one or more parts are > 255

--- 6kyu_IP_Validation/function.js
+++ 6kyu_IP_Validation/function.js
@@ -1,6 +1,12 @@
 function isValidIP(str) {
   let parts = str.split(".");
-  return parts.length === 4;
+  if (parts.length != 4) return false;
+  let validatedParts = validMap(parts);
+  return validatedParts.includes(false) ? false : true;
+}
+
+function validMap(parts) {
+  return parts.map((part) => (part <= 255 ? true : false));
 }
 
 module.exports = isValidIP;
--- 6kyu_IP_Validation/function.test.js
+++ 6kyu_IP_Validation/function.test.js
@@ -1,9 +1,17 @@
 const isValidIP = require("./function");
 
-test("It returns true when IP = 0.0.0.0", () => {
+test("Returns true when IP = 0.0.0.0", () => {
   expect(isValidIP("0.0.0.0")).toBe(true);
 });
 
-test("It returns false if not enough numbers are given", () => {
+test("Returns false if not enough numbers are given", () => {
   expect(isValidIP("0.0.0")).toBe(false);
+});
+
+test("Returns false if too many numbers are given", () => {
+  expect(isValidIP("0.0.0.0.0")).toBe(false);
+});
+
+test("Returns false when one or more parts are > 255", () => {
+  expect(isValidIP("0.0.0.256")).toBe(false);
 });
