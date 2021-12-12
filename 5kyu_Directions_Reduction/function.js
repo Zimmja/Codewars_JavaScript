02203ef90c0e2f457eb6e0e2ede10aec46e6eb13
Matt Zimmer <zimmermja@gmail.com>
2021-12-12 13:01:40 +0000

    passing test: Return an empty array if input array contains only cancelling elements

--- 5kyu_Directions_Reduction/function.js
+++ 5kyu_Directions_Reduction/function.js
@@ -1,5 +1,24 @@
 function dirReduc(arr) {
-  return arr;
+  let countN = arr.filter((x) => x === "NORTH").length;
+  let countS = arr.filter((x) => x === "SOUTH").length;
+  let countE = arr.filter((x) => x === "EAST").length;
+  let countW = arr.filter((x) => x === "WEST").length;
+  let resolveNS = resolveDir("NORTH", "SOUTH", countN, countS);
+  let resolveEW = resolveDir("EAST", "WEST", countE, countW);
+  return resolveNS.concat(resolveEW);
+}
+
+function resolveDir(up, down, countU, countD) {
+  countR = countU - countD;
+  if (countR === 0) {
+    return [];
+  } else if (countR > 0) {
+    let upArr = new Array(countR);
+    return upArr.fill(up);
+  } else {
+    let downArr = new Array(0 - countR);
+    return downArr.fill(down);
+  }
 }
 
 module.exports = dirReduc;
--- 5kyu_Directions_Reduction/function.test.js
+++ 5kyu_Directions_Reduction/function.test.js
@@ -12,3 +12,7 @@ test("Return the inpurt array if it contains no cancelling elements", () => {
   expect(dirReduc(["NORTH", "EAST"])).toEqual(["NORTH", "EAST"]);
   expect(dirReduc(["SOUTH", "WEST"])).toEqual(["SOUTH", "WEST"]);
 });
+
+test("Return an empty array if input array contains only cancelling elements", () => {
+  expect(dirReduc(["SOUTH", "NORTH"])).toEqual([]);
+});
