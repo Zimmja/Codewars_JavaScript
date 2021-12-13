07c6a53bac49d7f1565dda9a58d4aa78f014b377
Matt Zimmer <zimmermja@gmail.com>
2021-12-13 13:58:57 +0000

    Returns [[1, 1]. [42, 2500]] for 1 - 50

--- 5kyu_Integers:_Recreation_One/function.js
+++ 5kyu_Integers:_Recreation_One/function.js
@@ -1,5 +1,30 @@
 function listSquared(m, n) {
-  return true;
+  let squared = [];
+  for (let i = m; i < n; i++) {
+    let iDivisors = getDivisors(i);
+    if (sumHasRoot(iDivisors)) squared.push([i, sumSquares(iDivisors)]);
+  }
+  return squared;
+}
+
+function sumHasRoot(arr) {
+  let squareRoot = Math.sqrt(sumSquares(arr));
+  return squareRoot === parseInt(squareRoot);
+}
+
+function getDivisors(num) {
+  let divisors = [];
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) divisors.push(i);
+  }
+  return divisors;
+}
+
+function sumSquares(arr) {
+  let squares = arr.map((x) => x * x);
+  return squares.reduce((x, y) => x + y);
 }
 
 module.exports = listSquared;
+
+console.log(listSquared(1, 50));
--- 5kyu_Integers:_Recreation_One/function.test.js
+++ 5kyu_Integers:_Recreation_One/function.test.js
@@ -1,5 +1,12 @@
 const listSquared = require("./function");
 
-test("Setup", () => {
-  expect(listSquared()).toEqual(true);
+test("Returns [[1, 1]] for 1 - 10", () => {
+  expect(listSquared(1, 10)).toEqual([[1, 1]]);
+});
+
+test("Returns [[1, 1]. [42, 2500]] for 1 - 50", () => {
+  expect(listSquared(1, 50)).toEqual([
+    [1, 1],
+    [42, 2500],
+  ]);
 });
