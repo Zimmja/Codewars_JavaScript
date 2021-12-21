7258676659ea2b07185346fc28e272b4ab65cc0c
Matt Zimmer <zimmermja@gmail.com>
2021-12-21 15:11:55 +0000

    capitalizing words

--- 5kyu_The_Hashtag_Generator/function.js
+++ 5kyu_The_Hashtag_Generator/function.js
@@ -1,5 +1,11 @@
 function generateHashtag(str) {
-  return `#${str}`;
+  return `#${capitalize(str)}`;
+}
+
+function capitalize(str) {
+  return `${str.charAt(0).toUpperCase()}${str.substring(1)}`;
 }
 
 module.exports = generateHashtag;
+
+generateHashtag("hello");
--- 5kyu_The_Hashtag_Generator/function.test.js
+++ 5kyu_The_Hashtag_Generator/function.test.js
@@ -3,3 +3,7 @@ const generateHashtag = require("./function");
 test("Returns #Hello for Hello", () => {
   expect(generateHashtag("Hello")).toEqual("#Hello");
 });
+
+test("Returns #Hello for hello", () => {
+  expect(generateHashtag("hello")).toEqual("#Hello");
+});
