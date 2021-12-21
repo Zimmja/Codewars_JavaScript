0d4468322c81d0892d7217506cdceb0c8c9b0d4b
Matt Zimmer <zimmermja@gmail.com>
2021-12-21 14:46:57 +0000

    passign test for red

--- 5kyu_RGB_To_Hex_Conversion/function.js
+++ 5kyu_RGB_To_Hex_Conversion/function.js
@@ -1,5 +1,15 @@
 function rgb(r, g, b) {
-  return r + g + b === 0 ? "000000" : "FFFFFF";
+  return `${hexify(r)}${hexify(g)}${hexify(b)}`;
+}
+
+function hexify(dec) {
+  let hexa = dec.toString(16);
+  return formatHex(hexa);
+}
+
+function formatHex(hex) {
+  let prefix = hex.length === 1 ? "0" : "";
+  return `${prefix}${hex}`.toUpperCase();
 }
 
 module.exports = rgb;
--- 5kyu_RGB_To_Hex_Conversion/function.test.js
+++ 5kyu_RGB_To_Hex_Conversion/function.test.js
@@ -5,5 +5,9 @@ test("Black: returns 000000 for rgb(0,0,0)", () => {
 });
 
 test("White: returns FFFFFF for rgb(255,255,255)", () => {
-  expect(rgb(0, 0, 0)).toBe("000000");
+  expect(rgb(255, 255, 255)).toBe("FFFFFF");
+});
+
+test("Red: returns FF0000 for rgb(255,0,0)", () => {
+  expect(rgb(255, 0, 0)).toBe("FF0000");
 });
