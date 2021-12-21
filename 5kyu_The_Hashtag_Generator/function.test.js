8d9f00f55e8d85de4299aeb375a25a63afcb860d
Matt Zimmer <zimmermja@gmail.com>
2021-12-21 15:16:19 +0000

    added already-passing tests for multiple words and spaces

--- 5kyu_The_Hashtag_Generator/function.test.js
+++ 5kyu_The_Hashtag_Generator/function.test.js
@@ -11,3 +11,13 @@ test("Returns #Hello for hello", () => {
 test("Returns #HelloWorld for hello world", () => {
   expect(generateHashtag("hello world")).toEqual("#HelloWorld");
 });
+
+test("Returns #HelloWorldIsRunning for hello world is running", () => {
+  expect(generateHashtag("hello world is running")).toEqual(
+    "#HelloWorldIsRunning"
+  );
+});
+
+test("Returns #HelloWorld for hello    world", () => {
+  expect(generateHashtag("hello    world")).toEqual("#HelloWorld");
+});
