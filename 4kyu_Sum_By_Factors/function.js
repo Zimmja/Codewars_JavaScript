74a75d98512883a1f1eeb8aa492d7bdff61d753d
Matt Zimmer <zimmermja@gmail.com>
2021-12-22 13:06:18 +0000

    getPrimeFactors function created, passing first two tests

--- 4kyu_Sum_By_Factors/function.js
+++ 4kyu_Sum_By_Factors/function.js
@@ -1,6 +1,23 @@
 function sumOfDivided(lst) {
-  //your code
-  return [[2, 12]];
+  return [[getPrimeFactors(lst[0])[0], lst[0]]];
+}
+
+function getPrimeFactors(val) {
+  let factors = [];
+  let max = val;
+  let divisor = 2;
+
+  while (divisor >= 2 && divisor <= max / 2) {
+    if (val % divisor === 0) {
+      factors.push(divisor);
+      val /= divisor;
+    }
+    divisor++;
+  }
+
+  return factors;
 }
 
 module.exports = sumOfDivided;
+
+console.log(sumOfDivided([6]));
--- 4kyu_Sum_By_Factors/function.test.js
+++ 4kyu_Sum_By_Factors/function.test.js
@@ -1,5 +1,9 @@
 const sumOfDivided = require("./function");
 
-test("returns", () => {
-  expect(sumOfDivided([[2, 12]])).toEqual([[2, 12]]);
+test("A single value with one prime factor: 2", () => {
+  expect(sumOfDivided([4])).toEqual([[2, 4]]);
+});
+
+test("A single value with one prime factor: 3", () => {
+  expect(sumOfDivided([9])).toEqual([[3, 9]]);
 });
