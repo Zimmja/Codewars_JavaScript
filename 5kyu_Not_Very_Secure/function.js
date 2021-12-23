82be5e55c75d9cfc871c5868e62e7dfc9fafd1fa
Matt Zimmer <zimmermja@gmail.com>
2021-12-23 17:59:20 +0000

    passing kata

--- 5kyu_Not_Very_Secure/function.js
+++ 5kyu_Not_Very_Secure/function.js
@@ -1,6 +1,14 @@
 function alphanumeric(string) {
-  //your code here
-  return string.includes(" ") ? false : true;
+  if (string === "") return false;
+  let chars = string.split("");
+  let scan = chars.map((char) => charIsAlphNum(char));
+  return !scan.includes(false);
+}
+
+function charIsAlphNum(char) {
+  let alphNum =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  return alphNum.includes(char);
 }
 
 module.exports = alphanumeric;
--- 5kyu_Not_Very_Secure/function.test.js
+++ 5kyu_Not_Very_Secure/function.test.js
@@ -7,3 +7,25 @@ test("Returns true for simple words", () => {
 test("Returns false for a single whitespace", () => {
   expect(alphanumeric(" ")).toEqual(false);
 });
+
+test("Returns false for a combo of character and whitespace", () => {
+  expect(alphanumeric("a ")).toEqual(false);
+});
+
+test("Returns false for a single non-alphanumeric characters", () => {
+  expect(alphanumeric("!")).toEqual(false);
+  expect(alphanumeric("?")).toEqual(false);
+  expect(alphanumeric(")")).toEqual(false);
+  expect(alphanumeric("(")).toEqual(false);
+  expect(alphanumeric(".")).toEqual(false);
+});
+
+test("Returns true for complex passwords", () => {
+  expect(alphanumeric("hell0Wor1d")).toEqual(true);
+  expect(alphanumeric("0dvjN45eprSD54unIIgv9")).toEqual(true);
+  expect(alphanumeric("gr8m8wotASt8")).toEqual(true);
+});
+
+test("Returns false for blank string", () => {
+  expect(alphanumeric("")).toEqual(false);
+});
