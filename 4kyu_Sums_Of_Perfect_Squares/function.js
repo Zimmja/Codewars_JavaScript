a4982feaf30e792bacc766754c9c34e42548feab
Matt Zimmer <zimmermja@gmail.com>
2021-12-24 16:53:51 +0000

    passing second test with isSquare function

--- 4kyu_Sums_Of_Perfect_Squares/function.js
+++ 4kyu_Sums_Of_Perfect_Squares/function.js
@@ -1,5 +1,9 @@
 function sumOfSquares(n) {
-  return 1;
+  return isSquare(n) ? 1 : 2;
+}
+
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
 }
 
 module.exports = sumOfSquares;
--- 4kyu_Sums_Of_Perfect_Squares/function.test.js
+++ 4kyu_Sums_Of_Perfect_Squares/function.test.js
@@ -5,3 +5,8 @@ test("Returns 1 for numbers that are squares", () => {
   expect(sumOfSquares(4)).toEqual(1);
   expect(sumOfSquares(9)).toEqual(1);
 });
+
+test("Returns 2 for numbers that are squares + 1", () => {
+  expect(sumOfSquares(5)).toEqual(2);
+  expect(sumOfSquares(10)).toEqual(2);
+});
