228a02ff1e872560662e997124210bdc888d16f7
Matt Zimmer <zimmermja@gmail.com>
2021-12-26 20:17:02 +0000

    working for 5 and 5

--- 4kyu_Spinning_Rings/function.js
+++ 4kyu_Spinning_Rings/function.js
@@ -1,5 +1,24 @@
 function spinningRings(innerMax, outerMax) {
-  return 1;
+  let innerCount = 1;
+  let outerCount = outerMax;
+  let count = 1;
+  while (innerCount != outerCount && count != 100000) {
+    console.log(`Start spin, inner:${innerCount}, outer:${outerCount}`);
+    innerCount = innerCount === innerMax ? 0 : innerCount + 1;
+    outerCount = outerCount === 0 ? outerMax : outerCount - 1;
+    count += 1;
+  }
+  return count;
+}
+
+function getRange(max) {
+  let range = [];
+  for (let i = 0; i <= max; i++) {
+    range.push(i);
+  }
+  return range;
 }
 
 module.exports = spinningRings;
+
+console.log(spinningRings(5, 5));
--- 4kyu_Spinning_Rings/function.test.js
+++ 4kyu_Spinning_Rings/function.test.js
@@ -1,5 +1,13 @@
 const solution = require("./function");
 
-test("Return -1", () => {
+test("Return 1 for [1, 1]", () => {
   expect(solution(1, 1)).toEqual(1);
+});
+
+test("Return 3 for [2, 2]", () => {
+  expect(solution(2, 2)).toEqual(3);
+});
+
+test("Return 3 for [5, 5]", () => {
+  expect(solution(5, 5)).toEqual(3);
 });
