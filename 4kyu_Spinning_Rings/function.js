a2a77db9b153c9b93199c3986a6f2101a8cae460
Matt Zimmer <zimmermja@gmail.com>
2021-12-29 15:06:47 +0000

    further tests working, stuck on smaller outer ring with much bigger inner

--- 4kyu_Spinning_Rings/function.js
+++ 4kyu_Spinning_Rings/function.js
@@ -2,34 +2,47 @@ function spinningRings(iMax, oMax) {
   // return spinningRingsBF(iMax, outerMax);
   let count = 1, // Inner decreases, outer increases
     iCount = iMax,
-    oCount = 1;
+    oCount = 1,
+    cycles = 0,
+    cycleMax = 100000;
 
-  while (iCount != oCount) {
+  while (iCount != oCount && cycles < cycleMax) {
+    seeHERE(`Start spin, DOWN:${iCount}, UP:${oCount}`);
     let mod = 1; //minDif(outerCount, oMax, iCount);
     if (iCount < oCount) {
-      seeHERE("MISS [inner < outer]");
-      if (oCount <= iMax) mod = setMod("OUT < INmax", 1);
-      if (oCount > iMax) mod = setMod("IN < OUT", oMax - oCount + 1);
+      seeHERE("   MISS [DOWN < UP]");
+      if (oCount <= iMax) mod = setMod("UP <= DOWNmax", 1);
+      if (oCount > iMax) mod = setMod("UP > DOWNmax", oMax - oCount + 1);
     } else if ((iCount - oCount) % 2 != 0) {
-      seeHERE("MISS [passing by]");
-      if (oCount <= iMax) mod = setMod("OUT <= INmax", iCount + 1);
-      if (oCount > iMax) mod = setMod("OUT > INmax", oMax - oCount + 1);
+      seeHERE("   MISS [passing by]");
+      if (oCount <= iMax) {
+        let oLeft = oMax - oCount + 1;
+        let iLeft = iCount + 1;
+        mod =
+          iLeft > oLeft
+            ? setMod("UP <= DOWNmax, 0 outer", oLeft)
+            : setMod("UP <= DOWNmax, max inner", iLeft);
+      }
+      if (oCount > iMax) mod = setMod("UP > DOWNmax", oMax - oCount + 1);
     } else if ((iCount - oCount) % 2 == 0) {
       mod = setMod("HIT", iCount - oCount) / 2; // Inner and outer will meet on this spin, go to meeting place
     }
-    seeHERE(`  Start spin, IN:${iCount}, OUT:${oCount}, mod:${mod}`);
+    seeHERE(`       mod:${mod}`);
     iCount -= mod;
     oCount += mod;
     count += mod;
+    seeHERE(`       Postmod spin, DOWN:${iCount}, UP:${oCount}`);
 
-    iCount = checkBorders(iCount, iMax);
-    oCount = checkBorders(oCount, oMax);
+    iCount = iResolve(iCount, iMax);
+    oCount = oResolve(oCount, oMax);
+    seeHERE(`          Finish spin, DOWN:${iCount}, UP:${oCount}`);
+    cycles += 1;
   }
-  return count;
+  return cycles === cycleMax ? "Timeout" : count;
 }
 
 function setMod(message, val) {
-  seeHERE(message);
+  seeHERE(`     ${message}`);
   return val;
 }
 
@@ -37,9 +50,22 @@ function seeHERE(message) {
   // console.log(message);
 }
 
+function iResolve(val, max) {
+  if (val <= max && val >= 0) return val;
+  let spill = ((0 - val + 1) % (max + 1)) - 1;
+  return spill > 0 ? max - (spill - 1) : 0 - spill;
+}
+
+function oResolve(val, max) {
+  if (val <= max && val >= 0) return val;
+  let spill = ((val + 1) % (max + 1)) - 1;
+  return spill === -1 ? max : spill;
+}
+
 function checkBorders(val, maxVal) {
   if (val < 0) val = maxVal + (val % (maxVal + 1)) + 1;
   if (val > maxVal) val = (val % maxVal) - 1;
+  if (val < 0) val = 0 - val;
   return val;
 }
 
@@ -49,11 +75,13 @@ console.log(`FINAL: ${spinningRings(1, 1)}`); // 1
 console.log(`FINAL: ${spinningRings(2, 2)}`); // 3
 console.log(`FINAL: ${spinningRings(5, 5)}`); // 3
 console.log(`FINAL: ${spinningRings(2, 10)}`); // 13
-// console.log(`FINAL: ${spinningRings(16777216, 14348907)}`); // 23951671
-// console.log(`FINAL: ${spinningRings(131983531646778, 90915287537703)}`); // 177441175415631
-// console.log(`FINAL: ${spinningRings(5362, 253199320071448)}`);
+console.log(`FINAL: ${spinningRings(10, 2)}`); // 10
+console.log(`FINAL: ${spinningRings(16777216, 14348907)}`); // 23951671
+console.log(`FINAL: ${spinningRings(131983531646778, 90915287537703)}`); // 177441175415631
+console.log(`FINAL: ${spinningRings(5362, 253199320071448)}`); // 253199320072568
+console.log(`FINAL: ${spinningRings(5028, 845)}`); // 9259
 
-// console.log(`FINAL: ${spinningRingsBF(2, 2)}`); // 13
+// console.log(`FINAL: ${spinningRingsBF(10, 2)}`); // 13
 
 // OLD FUNCTS
 // if (innerCount === 0) {
--- 4kyu_Spinning_Rings/function.test.js
+++ 4kyu_Spinning_Rings/function.test.js
@@ -4,26 +4,39 @@ test("Return 1 for [1, 1]", () => {
   expect(solution(1, 1)).toEqual(1);
 });
 
-test("Return 3 for [2, 2]", () => {
+test("Simple inputs with identical inners and outers [2, 2]", () => {
   expect(solution(2, 2)).toEqual(3);
-});
-
-test("Return 3 for [5, 5]", () => {
   expect(solution(5, 5)).toEqual(3);
 });
 
-test("Return 13 for [2, 10]", () => {
+test("Simple inputs with higher outer value [2, 10]", () => {
   expect(solution(2, 10)).toEqual(13);
 });
 
-test("Works for larger values", () => {
-  expect(solution(2 ** 24, 3 ** 15)).toEqual(23951671);
+test("Simple inputs with higher innrt value [10, 2]", () => {
+  expect(solution(10, 2)).toEqual(10);
 });
 
-test("Works for really big values", () => {
+test("Larger values of similar size", () => {
+  expect(solution(16777216, 14348907)).toEqual(23951671);
+});
+
+test("Very large values of similar size", () => {
   expect(solution(131983531646778, 90915287537703)).toEqual(177441175415631);
 });
 
-// test("Works for really big values paired with smaller values", () => {
-//   expect(solution(5362, 253199320071448)).toEqual("?");
-// });
+test("Smaller inner, small difference", () => {
+  expect(solution(845, 5028)).toEqual(423);
+});
+
+test("Smaller inner, huge difference", () => {
+  expect(solution(5362, 253199320071448)).toEqual(253199320072568);
+});
+
+test("Smaller outer, small difference", () => {
+  expect(solution(5028, 845)).toEqual(9259);
+});
+
+test("Smaller outer, huge difference", () => {
+  expect(solution(84728478560236, 6635)).toEqual("?");
+});
