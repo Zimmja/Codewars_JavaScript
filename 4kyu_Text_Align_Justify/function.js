ea5a8f482e25405af403a53d2140b0388c099970
Matt Zimmer <zimmermja@gmail.com>
2021-12-31 13:26:01 +0000

    joining two words

--- 4kyu_Text_Align_Justify/function.js
+++ 4kyu_Text_Align_Justify/function.js
@@ -1,5 +1,17 @@
 function justify(str, len) {
-  return str;
+  let words = str.split(" ");
+  let wordLengths = words.map((word) => word.length);
+  if (wordLengths.length === 1) return str;
+  let spaceRequired = len - sumArr(wordLengths);
+  return words.join(spaces(spaceRequired));
+}
+
+function spaces(count) {
+  return new Array(count + 1).join(" ");
+}
+
+function sumArr(arr) {
+  return arr.reduce((a, b) => a + b);
 }
 
 module.exports = justify;
--- 4kyu_Text_Align_Justify/function.test.js
+++ 4kyu_Text_Align_Justify/function.test.js
@@ -1,5 +1,9 @@
 const justify = require("./function");
 
-test("When the string is shorter than len, return string", () => {
+test("When the string is one word and shorter than len, return string", () => {
   expect(justify("Hello", 10)).toEqual("Hello");
+});
+
+test("When the string is two words and shorter than len, justify to length", () => {
+  expect(justify("Hello world!", 15)).toEqual("Hello    world!");
 });
