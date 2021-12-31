6614f709428d7be4dc7b10e33342eea68d136793
Matt Zimmer <zimmermja@gmail.com>
2021-12-31 13:59:09 +0000

    words for uneven nmber of spaces

--- 4kyu_Text_Align_Justify/function.js
+++ 4kyu_Text_Align_Justify/function.js
@@ -2,11 +2,34 @@ function justify(str, len) {
   let words = str.split(" ");
   let wordLengths = words.map((word) => word.length);
   if (wordLengths.length === 1) return str;
-  let spaceRequired = len - sumArr(wordLengths);
-  return words.join(spaces(spaceRequired));
+  let space = spaceNeeded(len, wordLengths);
+  if (space.spill === 0) {
+    return words.join(buildSpace(space.size));
+  } else {
+    let spaces = new Array(space.count).fill(buildSpace(space.size));
+    for (let i = 0; i < space.spill; i++) {
+      spaces[i] = spaces[i] + " ";
+    }
+    let output = [];
+    let cycles = words.length * 2 - 1;
+    for (let j = 0; j < cycles; j++) {
+      if (j % 2 === 0) output.push(words.shift());
+      if (j % 2 != 0) output.push(spaces.shift());
+    }
+    return output.join("");
+  }
 }
 
-function spaces(count) {
+function spaceNeeded(lineLength, wordLengths) {
+  let wordCount = wordLengths.length;
+  let spaceCount = wordCount - 1;
+  let spaceLeft = lineLength - sumArr(wordLengths);
+  let spaceSize = parseInt(spaceLeft / spaceCount);
+  let spaceSpill = lineLength - spaceSize * spaceCount - sumArr(wordLengths);
+  return { size: spaceSize, spill: spaceSpill, count: spaceCount };
+}
+
+function buildSpace(count) {
   return new Array(count + 1).join(" ");
 }
 
--- 4kyu_Text_Align_Justify/function.test.js
+++ 4kyu_Text_Align_Justify/function.test.js
@@ -7,3 +7,12 @@ test("When the string is one word and shorter than len, return string", () => {
 test("When the string is two words and shorter than len, justify to length", () => {
   expect(justify("Hello world!", 15)).toEqual("Hello    world!");
 });
+
+test("String is three words, shorter than len, evenly spaced", () => {
+  expect(justify("Hello there world", 21)).toEqual("Hello   there   world");
+});
+
+test("String is three words, shorter than len, unevenly spaced", () => {
+  expect(justify("Hello there world", 22)).toEqual("Hello    there   world");
+  expect(justify("How now red cow bow", 22)).toEqual("How  now  red  cow bow");
+});
