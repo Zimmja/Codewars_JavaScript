9025c7405f088ddc8ac662ecd01adc7633beffc3
Matt Zimmer <zimmermja@gmail.com>
2022-01-26 21:16:55 +0000

    minor refactor

--- 7kyu_Regexp_Basics/function.js
+++ 7kyu_Regexp_Basics/function.js
@@ -1,5 +1,5 @@
 const eightBit = function (number) {
-  return /^([1-9]?\d|1\d\d|2([0-5][0-5]|[0-4]\d))$/.test(number);
+  return /^([1-9]?\d|1\d\d|2(5[0-5]|[0-4]\d))$/.test(number);
 };
 
 String.prototype.eightBitNumber = function () {
