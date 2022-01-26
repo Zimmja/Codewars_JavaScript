fd6aacfff55edff8c72840dab5bba72483d4d228
Matt Zimmer <zimmermja@gmail.com>
2022-01-26 20:59:42 +0000

    refactored regex basics

--- 7kyu_Regexp_Basics/function.js
+++ 7kyu_Regexp_Basics/function.js
@@ -1,5 +1,3 @@
-const eightBit = (number) => {
-  return /(^[1-9]?\d$)|(^1\d\d$)|(^2[0-5][0-5]$)/.test(number);
-};
+const eightBit = (number) => /^([1-9]?\d|1\d\d|2[0-5][0-5])$/.test(number);
 
 module.exports = eightBit;
