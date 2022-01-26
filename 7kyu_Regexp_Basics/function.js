25817acfa176f2d1a3931c9db45458b45714200a
Matt Zimmer <zimmermja@gmail.com>
2022-01-26 15:38:49 +0000

    all tests passing, covers 0 - 256

--- 7kyu_Regexp_Basics/function.js
+++ 7kyu_Regexp_Basics/function.js
@@ -1,5 +1,6 @@
-const eightBit = () => {
-  return true;
+const eightBit = (number) => {
+  const regex = /(^\d$)|(^[1-9]\d$)|(^1\d\d$)|(^2[0-5][0-5]$)/;
+  return regex.test(number);
 };
 
 module.exports = eightBit;
--- 7kyu_Regexp_Basics/function.test.js
+++ 7kyu_Regexp_Basics/function.test.js
@@ -1,5 +1,29 @@
 const eightBit = require("./function.js");
 
-test("returns true", () => {
-  expect(eightBit()).toEqual(true);
+test("returns true for 0", () => {
+  expect(eightBit("0")).toEqual(true);
+});
+
+test("returns false for 00", () => {
+  expect(eightBit("00")).toEqual(false);
+});
+
+test("returns true for 10", () => {
+  expect(eightBit("10")).toEqual(true);
+});
+
+test("returns true for 100", () => {
+  expect(eightBit("100")).toEqual(true);
+});
+
+test("returns true for 1", () => {
+  expect(eightBit("1")).toEqual(true);
+});
+
+test("returns true for 255", () => {
+  expect(eightBit("255")).toEqual(true);
+});
+
+test("returns false for 256", () => {
+  expect(eightBit("256")).toEqual(false);
 });
