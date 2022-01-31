f36e02499350cbd12f93a305c22d80880f7f18a2
Matt Zimmer <zimmermja@gmail.com>
2022-01-31 16:15:10 +0000

    first test passing

--- 4kyu_4_By_4_Skyscrapers/function.js
+++ 4kyu_4_By_4_Skyscrapers/function.js
@@ -1,5 +1,32 @@
 function solvePuzzle(clues) {
-  return true;
+  const block = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+  const blockCheckedFours = checkFours(clues, block);
+  return blockCheckedFours;
 }
 
+const checkFours = (clues, block) => {
+  clues.forEach((clue, i) => {
+    if (clue == 4 && i <= 3) {
+      block = fillColumn(block, i, [1, 2, 3, 4]);
+    }
+  });
+  return block;
+};
+
+const fillColumn = (block, colIndx, content, reverse = false) => {
+  if (reverse) content.reverse();
+  return block.map((row, i) => {
+    row[colIndx] = content[i];
+    return row;
+  });
+};
+
 module.exports = solvePuzzle;
+
+const puzzle01 = [4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+console.log(solvePuzzle(puzzle01));
--- 4kyu_4_By_4_Skyscrapers/function.test.js
+++ 4kyu_4_By_4_Skyscrapers/function.test.js
@@ -1,5 +1,12 @@
 const solvePuzzle = require("./function.js");
 
+const puzzle01 = [4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
 test("returns true", () => {
-  expect(solvePuzzle(1)).toEqual(true);
+  expect(solvePuzzle(puzzle01)).toEqual([
+    [1, 1, 1, 1],
+    [2, 2, 2, 2],
+    [3, 3, 3, 3],
+    [4, 4, 4, 4],
+  ]);
 });
