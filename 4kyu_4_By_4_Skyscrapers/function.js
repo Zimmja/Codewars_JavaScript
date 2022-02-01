85d3572432045f669c93ad2073495a26ba69abe7
Matt Zimmer <zimmermja@gmail.com>
2022-02-01 15:40:10 +0000

    refactored row and columns functions into a single function

--- 4kyu_4_By_4_Skyscrapers/function.js
+++ 4kyu_4_By_4_Skyscrapers/function.js
@@ -19,63 +19,53 @@ function solvePuzzle(clues) {
 
 const sumArr = (arr) => arr.reduce((a, b) => a + b);
 
-const setSquare = (block, rowIndx, colIndx, setVal) => {
-  let blockClone = block.slice();
-  if (blockClone[rowIndx][colIndx] != 0) return block;
-  blockClone[rowIndx][colIndx] = setVal;
-  return blockClone;
-};
-
-const setColumn = (block, colIndx, setVals) => {
-  let blockClone = block.slice();
-  const colVals = [
-    blockClone[0][colIndx],
-    blockClone[1][colIndx],
-    blockClone[2][colIndx],
-    blockClone[3][colIndx],
-  ];
-  if (colVals === setVals) return block;
-  setVals.forEach((setVal, i) => {
-    setSquare(block, i, colIndx, setVal);
-  });
-  return blockClone;
-};
-
-const setRow = (block, rowIndx, setVals) => {
-  let blockClone = block.slice();
-  const rowVals = blockClone[rowIndx];
-  if (rowVals === setVals) return block;
-  setVals.forEach((setVal, i) => {
-    setSquare(block, rowIndx, i, setVal);
-  });
-  return blockClone;
-};
-
 const checkAgainstRules = (clues, block) => {
-  return checkFours(clues, block);
+  let blockClone = block.slice();
+  blockClone = checkFours(clues, blockClone);
+  // blockClone = checkOnes(clues, blockClone);
+  return blockClone;
 };
+
+// const checkOnes = (clues, block)
 
 const checkFours = (clues, block) => {
-  let newBlock = block.slice();
   clues.forEach((clue, i) => {
     if (clue == 4) {
       if (i >= 12) {
-        console.log("Updating over 12");
-        newBlock = setRow(newBlock, 3 - (i - 12), [1, 2, 3, 4]);
+        block = setRowOrCol(block, 3 - (i - 12), [1, 2, 3, 4]);
       } else if (i >= 8) {
-        console.log("Updating over 8");
-        newBlock = setColumn(newBlock, 3 - (i - 8), [4, 3, 2, 1]);
+        block = setRowOrCol(block, 3 - (i - 8), [4, 3, 2, 1], false);
       } else if (i >= 4) {
-        console.log("Updating over 4");
-        newBlock = setRow(newBlock, i - 4, [4, 3, 2, 1]);
+        block = setRowOrCol(block, i - 4, [4, 3, 2, 1]);
       } else {
-        console.log("Updating over 0");
-        newBlock = setColumn(newBlock, i, [1, 2, 3, 4]);
+        block = setRowOrCol(block, i, [1, 2, 3, 4], false);
       }
     }
   });
-  return newBlock;
+  return block;
 };
+
+const setSquare = (block, rowIndx, colIndx, setVal) => {
+  if (block[rowIndx][colIndx] != 0) return block;
+  block[rowIndx][colIndx] = setVal;
+  return block;
+};
+
+const setRowOrCol = (block, index, setVals, row = true) => {
+  const vals = row ? block[index] : getColVals(block, index);
+  if (vals === setVals) return block;
+  setVals.forEach((setVal, i) => {
+    setSquare(block, row ? index : i, row ? i : index, setVal);
+  });
+  return block;
+};
+
+const getColVals = (block, colIndx) => [
+  block[0][colIndx],
+  block[1][colIndx],
+  block[2][colIndx],
+  block[3][colIndx],
+];
 
 module.exports = solvePuzzle;
 
