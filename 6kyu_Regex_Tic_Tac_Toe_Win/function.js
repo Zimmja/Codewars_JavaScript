7a412ba56f5be99948ee6d923a03dd4727b2b8dc
Matt Zimmer <zimmermja@gmail.com>
2022-02-03 16:43:41 +0000

    rewrote code to allow a character count; regex down to 111

--- 6kyu_Regex_Tic_Tac_Toe_Win/function.js
+++ 6kyu_Regex_Tic_Tac_Toe_Win/function.js
@@ -1,15 +1,22 @@
 function regexTicTacToeWinChecker(board) {
-  const row01 = /(XXX|OOO)....../.test(board);
-  const row02 = /...(XXX|OOO).../.test(board);
-  const row03 = /......(XXX|OOO)/.test(board);
-  const colsO = /O..O..O/.test(board);
-  const colsX = /X..X..X/.test(board);
-  const diagX = /(..X.X.X..)|(X...X...X)/.test(board);
-  const diagO = /(..O.O.O..)|(O...O...O)/.test(board);
+  const row01 = /(XXX|OOO).{6}/;
+  const row02 = /...(XXX|OOO).../;
+  const row03 = /.{6}(XXX|OOO)/;
+  const colsO = /O..O..O/;
+  const colsX = /X..X..X/;
+  const diag1 = /(..(X.X.X|O.O.O)..)/;
+  const diag2 = /(X...X...X)|(O...O...O)/;
 
-  console.log(row01, row02, row03, colsO, colsX, diagX, diagO);
+  const regexes = [row01, row02, row03, colsO, colsX, diag1, diag2];
+  const regexTests = regexes.map((regex) => regex.test(board));
 
-  return [row01, row02, row03, colsO, colsX, diagX, diagO].includes(true);
+  // console.log(regexTests);
+  console.log(
+    "Regex length: " +
+      regexes.map((reg) => reg.toString().length).reduce((a, b) => a + b)
+  );
+
+  return regexTests.includes(true);
 }
 
 module.exports = regexTicTacToeWinChecker;
