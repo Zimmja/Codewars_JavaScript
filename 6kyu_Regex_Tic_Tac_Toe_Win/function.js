087533bf90c0af4223f037be8bb21c7d9d63ff0f
Matt Zimmer <zimmermja@gmail.com>
2022-02-03 16:03:07 +0000

    working for rows and columns

--- 6kyu_Regex_Tic_Tac_Toe_Win/function.js
+++ 6kyu_Regex_Tic_Tac_Toe_Win/function.js
@@ -1,5 +1,7 @@
 const regexTicTacToeWinChecker = (board) => {
-  return true;
+  return /([XO]{3}[-XO]{6})|([-XO]{3}[XO]{3}[-XO]{3})|([-XO]{6}[XO]{3})|(O[-XO]{2}O[-XO]{2}O)/.test(
+    board
+  );
 };
 
 module.exports = regexTicTacToeWinChecker;
--- 6kyu_Regex_Tic_Tac_Toe_Win/function.test.js
+++ 6kyu_Regex_Tic_Tac_Toe_Win/function.test.js
@@ -1,5 +1,36 @@
 const regexTicTacToeWinChecker = require("./function.js");
 
-test("Returns true", () => {
-  expect(regexTicTacToeWinChecker("hi")).toEqual(true);
+const emptyBoard = "---------";
+test("Returns false for an empty board", () => {
+  expect(regexTicTacToeWinChecker(emptyBoard)).toEqual(false);
+});
+
+const winX01 = "XXX------";
+test("Returns true when the top row is full of Xs", () => {
+  expect(regexTicTacToeWinChecker(winX01)).toEqual(true);
+});
+
+const winX02 = "---XXX---";
+test("Returns true when the middle row is full of Xs", () => {
+  expect(regexTicTacToeWinChecker(winX02)).toEqual(true);
+});
+
+const loose01 = "-XX------";
+test("Returns false when the top row is almost (but not) full of Xs", () => {
+  expect(regexTicTacToeWinChecker(loose01)).toEqual(false);
+});
+
+const loose02 = "-XXX-----";
+test("Returns false when there are three Xs in a row in the string, but not the board", () => {
+  expect(regexTicTacToeWinChecker(loose02)).toEqual(false);
+});
+
+const winO01 = "------OOO";
+test("Returns true when the last row is full of Os", () => {
+  expect(regexTicTacToeWinChecker(winO01)).toEqual(true);
+});
+
+const winO02 = "-O--O--O-";
+test("Returns true when a column of Os is present", () => {
+  expect(regexTicTacToeWinChecker(winO02)).toEqual(true);
 });
